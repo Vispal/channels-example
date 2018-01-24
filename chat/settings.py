627c667957b03354c1dd179a32169f76304eb267
jussi <jussi@virta.net>
2018-01-24 13:10:53 +0200

    Testing

--- chat/settings.py
+++ chat/settings.py
@@ -84,6 +84,8 @@ USE_TZ = True
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
+WSGI_APPLICATION = 'chat.wsgi.application'
+
 # Allow all host headers
 ALLOWED_HOSTS = ['*']
 
